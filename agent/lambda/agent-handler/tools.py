af54a074d0a14c9c704f64c9a24d1d98aafa93f7
mahendrabairagi <40216140+mahendrabairagi@users.noreply.github.com>
2023-10-19 08:43:26 -0700

    Update tools.py
    updated prompt to follow \n\nHuman: \n\nAssistant structure

--- agent/lambda/agent-handler/tools.py
+++ agent/lambda/agent-handler/tools.py
@@ -38,14 +38,14 @@ class Tools():
             return_source_documents=True
         )
 
-        prompt_template = """
+        prompt_template = """\n\nHuman:
         The following is a friendly conversation between a human and an AI. 
         The AI is talkative and provides lots of specific details from its context.
         If the AI does not know the answer to a question, it truthfully says it 
         does not know.
         {context}
         Instruction: Based on the above documents, provide a detailed answer and source document for, {question} Answer "don't know" if not present in the document. Solution:
-        """
+        \n\nAssistant:"""
 
         PROMPT = PromptTemplate(
           template=prompt_template, input_variables=["context", "question"]
